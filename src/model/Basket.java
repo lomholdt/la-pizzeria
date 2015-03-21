6a6a21bf71820e85b4c2f24252f9498bb2905c1e
jonaslomholdt <jonaslomholdt@gmail.com>
2015-03-21 10:25:25 +0100

    Updated basket

--- src/model/Basket.java
+++ src/model/Basket.java
@@ -8,6 +8,7 @@ public class Basket {
 	
 	public Map<Integer, Item> basket = new HashMap<Integer, Item>();
 	private int totalPrice;
+	private int quantity;
 	
 	public void add(Item i){
 		if(basket.containsKey(i.getId())){
@@ -18,16 +19,18 @@ public class Basket {
 			basket.put(i.getId(), i);		
 		}
 		totalPrice += i.getPrice();
+		quantity++;
 	}
 	
 	public void remove(int id){
 		Item p = basket.get(id);
 		totalPrice -= p.getPrice()*p.getQuantity();
 		basket.remove(id); // or basket.remove(p);
+		quantity -= p.getQuantity();
 	}
 
 	public int getSize() {
-		return basket.size();
+		return quantity;
 	}
 	
 	public Map<Integer, Item> getList(){
